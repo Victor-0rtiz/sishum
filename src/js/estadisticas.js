05e84865808eb96e655807fda1032e1fde59a311
victor <vicjosneros@gmail.com>
2023-11-10 22:48:30 -0600

    Agregada nuevas pantallas y edicion de tablas

--- src/js/estadisticas.js
+++ src/js/estadisticas.js
@@ -8,40 +8,38 @@
  // Configuración del gráfico de barras multi-axis
  const ctx = document.getElementById("multiAxisBarChart").getContext("2d");
  const multiAxisBarChart = new Chart(ctx, {
-     type: "bar",
-     data: {
-         labels: years,
-         datasets: [
-             {
-                 label: "Cantidad de Estudiantes",
-                 data: studentsCount,
-                 backgroundColor: "rgba(75, 192, 192, 0.6)",
-                 borderColor: "rgba(75, 192, 192, 1)",
-                 yAxisID: "y-axis-1", // Asignar al primer eje
-             },
-             {
-                 label: "Cantidad de Docentes",
-                 data: teachersCount,
-                 backgroundColor: "rgba(255, 99, 132, 0.6)",
-                 borderColor: "rgba(255, 99, 132, 1)",
-                 yAxisID: "y-axis-2", // Asignar al segundo eje
-             },
-         ],
-     },
-     options: {
-         scales: {
-             y: [
-                 {
-                     id: "y-axis-1",
-                     beginAtZero: true,
-                     position: "left", // Posición del eje izquierdo
-                 },
-                 {
-                     id: "y-axis-2",
-                     beginAtZero: true,
-                     position: "right", // Posición del eje derecho
-                 },
-             ],
-         },
-     },
- });
+    type: "bar",
+    data: {
+        labels: years,
+        datasets: [
+            {
+                label: "Cantidad de Estudiantes",
+                data: studentsCount,
+                backgroundColor: "rgba(75, 192, 192, 0.6)",
+                borderColor: "rgba(75, 192, 192, 1)",
+                yAxisID: "y1", // Asignar al primer eje
+            },
+            {
+                label: "Cantidad de Docentes",
+                data: teachersCount,
+                backgroundColor: "rgba(255, 99, 132, 0.6)",
+                borderColor: "rgba(255, 99, 132, 1)",
+                yAxisID: "y2", // Asignar al segundo eje
+            },
+        ],
+    },
+    options: {
+        scales: {
+            y1: {
+                id: "y1",
+                beginAtZero: true,
+                position: "left", // Posición del eje izquierdo
+            },
+            y2: {
+                id: "y2",
+                beginAtZero: true,
+                position: "right", // Posición del eje derecho
+            },
+        },
+    },
+});
