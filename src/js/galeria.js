c6eecc67d83ece7e9eafd4260b74dfa6434410c5
victor <vicjosneros@gmail.com>
2023-11-15 22:47:43 -0600

    Modificacion a la vista movil

--- src/js/galeria.js
+++ src/js/galeria.js
@@ -1,40 +1,60 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const mobileMenuIcon = document.querySelector(".navegacion__mobile-menu-icon i");
+  const mobileMenu = document.querySelector(".navegacion__div--url");
+  const mobileMenuLinks = document.querySelectorAll(".navegacion__div--url a");
+  const elementsToHideOnMobileMenu = document.querySelectorAll(".elementos-a-ocultar");
+
+  mobileMenuIcon.addEventListener("click", toggleMobileMenu);
+
+  mobileMenuLinks.forEach((link) => {
+    link.addEventListener("click", hideMobileMenu);
+  });
+
+  function toggleMobileMenu() {
+    mobileMenu.classList.toggle("show");
+
+    elementsToHideOnMobileMenu.forEach((element) => {
+      element.classList.toggle("hide-on-mobile-menu");
+    });
+  }
+
+  function hideMobileMenu() {
+    mobileMenu.classList.remove("show");
+
+    elementsToHideOnMobileMenu.forEach((element) => {
+      element.classList.remove("hide-on-mobile-menu");
+    });
+  }
+
   const imagenes = document.querySelectorAll(".galeria__imagenes img");
 
   imagenes.forEach((img) => {
     img.addEventListener("click", (e) => {
       abrirModal(e.target.src);
+      hideMobileMenu();
     });
   });
 
   function abrirModal(src) {
-    // Crear el elemento div y añadirle la clase 'modal'
-    const modal = document.createElement('div');
-    modal.classList.add('modal');
+    const modal = document.createElement("div");
+    modal.classList.add("modal");
 
-    // Crear la imagen y establecer su atributo src
-    const imagenModal = document.createElement('img');
+    const imagenModal = document.createElement("img");
     imagenModal.src = src;
 
-    // Crear el icono de cierre
-    const iconoCierre = document.createElement('i');
-    iconoCierre.classList.add('fas', 'fa-times', 'icono-cierre');
+    const iconoCierre = document.createElement("i");
+    iconoCierre.classList.add("fas", "fa-times", "icono-cierre");
 
-    // Agregar un evento de click al icono de cierre para cerrar el modal
-    iconoCierre.addEventListener('click', () => {
-        document.body.removeChild(modal);
-        document.body.style.overflow = 'auto'; // Aquí cambiamos overflow a 'auto' cuando el modal se cierre
+    iconoCierre.addEventListener("click", () => {
+      document.body.removeChild(modal);
+      document.body.style.overflow = "auto";
     });
 
-    // Añadir la imagen y el icono de cierre al modal
     modal.appendChild(imagenModal);
     modal.appendChild(iconoCierre);
 
-    // Añadir el modal al body del documento
     document.body.appendChild(modal);
 
-    // Cambiar overflow a 'hidden' cuando el modal se abra
-    document.body.style.overflow = 'hidden';
-}
-
+    document.body.style.overflow = "hidden";
+  }
 });
